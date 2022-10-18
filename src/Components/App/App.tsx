3874d5aa5b47bfd7d6975ce9c64d379cd71e7e01
Polina Kuznetsova <kuzneechik@gmail.com>
2022-10-18 22:41:30 +0300

    feat: added images, main layouts

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 
 import './App.scss';
 import List from '../List/List';
-import Main from '../Main/Main';
+import Main from '../Tasks/Tasks';
 
 const App = () => {
   return (
-    <div className="App">
-      <List />
-      <Main />
+    <div className='app container'>
+      <div className='app__sidebar'>
+        <List />
+      </div>
+      <div className='app__tasks'>
+        <Main />
+      </div>
     </div>
   );
 }
--- src/Components/Tasks/Tasks.tsx
+++ src/Components/Tasks/Tasks.tsx
@@ -1,4 +1,4 @@
-import './Main';
+import './Tasks';
 
 const Main = () => {
     return(
