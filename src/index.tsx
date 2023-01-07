4a98954ef518154dd1fabbd2134aa73ed91a5924
Polly2000 <kuznetsovapolina2020@gmail.com>
2023-01-08 00:47:52 +0300

    fix: rename folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './Components/App/App';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
