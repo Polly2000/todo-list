ecb0ab5e304766286cb67948c589c1bad17249c7
Kuznetsova Polina <kuznetsovapolina2020@gmail.com>
2023-01-18 18:22:44 +0300

    feat: registration in progress

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,9 +1,50 @@
+import { useState } from "react";
 import { Typography, TextField, Paper } from "@mui/material";
 import { Link } from 'react-router-dom';
+import axios from "axios";
 
 import './Registration.scss';
 
+// axios.defaults.withCredentials = true;
+
 const Registration = () => {
+
+  const [userName, setUserName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleName = (name: string) => {
+    setUserName(name);
+    console.log(name);
+  }
+
+  const handleEmail = (email: string) => {
+    setEmail(email);
+    console.log(email);
+  }
+
+  const handlePassword = (password: string) => {
+    setPassword(password);
+    console.log(password);
+  }
+
+  // const registerHandler = async () => {
+  //   try {
+  //     await axios
+  //             .post('http://localhost:5000/api/auth/registration', {
+  //               userName: userName,
+  //               email: email,
+  //               password: password
+  //             }, {
+  //               headers: {
+  //                 'Content-Type': 'application/json'
+  //               }
+  //             })
+  //             .then(response => console.log(response))
+  //   } catch(err) {
+  //     console.log(err);
+  //   }
+  // }
 
   return(
     <div className="containerRegistration">
@@ -20,17 +61,30 @@ const Registration = () => {
           label="Твоё имя"
           fullWidth
           color='success'
+          onChange={(e) => handleName(e.target.value)}
         />
         <TextField
           className='fieldRegistration'
           label="Твой email"
           fullWidth
           color='success'
+          onChange={(e) => handleEmail(e.target.value)}
         />
-        <TextField className='fieldRegistration' label="Введи пароль" color='success' type="password" autoComplete="current-password" fullWidth />
-        <TextField className='fieldRegistration' label="Повтори пароль" color='success' type="password" autoComplete="current-password" fullWidth />
+        <TextField 
+          className='fieldRegistration' 
+          label="Введи пароль" 
+          color='success' 
+          type="password" 
+          autoComplete="current-password" 
+          fullWidth
+          onChange={(e) => handlePassword(e.target.value)}
+        />
+        {/* <TextField className='fieldRegistration' label="Повтори пароль" color='success' type="password" autoComplete="current-password" fullWidth /> */}
         <div className='flex'>
-          <button className='button'>
+          <button 
+            className='button' 
+            // onClick={registerHandler}
+          >
             Зарегестрироваться
           </button>
           <Link to='/login' className='link'>
